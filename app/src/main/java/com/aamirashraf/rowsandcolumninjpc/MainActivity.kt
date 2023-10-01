d28c57b6bdf3211a507b42155955648c4715abe4
Android Dev <aamirnasa97@gmail.com>
2023-10-01 23:42:45 +0530

    all about row scope

--- app/src/main/java/com/aamirashraf/rowsandcolumninjpc/MainActivity.kt
+++ app/src/main/java/com/aamirashraf/rowsandcolumninjpc/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,15 +27,28 @@ class MainActivity : ComponentActivity() {
             //align item in crosss axix
 
             //alignment start/end also
-            Column(modifier = Modifier.fillMaxSize()
+//            Column(modifier = Modifier.fillMaxSize()
+//                .background(color = Color.Green),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+////                verticalArrangement = Arrangement.Center
+////            verticalArrangement = Arrangement.SpaceAround   //here at the end space is half as space in b/w
+////            verticalArrangement = Arrangement.SpaceBetween    //here is one colum at one end(top) and other at bottom
+//            verticalArrangement = Arrangement.SpaceEvenly   //equal space all round
+//            ) {
+//                Text(text = "hello world")
+//                Text(text = "Jetpack Compose")
+//            }
+            //now we are in row so the main axix for the row is the horizontal axis
+            //and the cross axis for the row is vertical
+            //so in case of the row axix the arragement is horizontal and alignment is vertical
+            Row(modifier = Modifier
+                .fillMaxSize()
                 .background(color = Color.Green),
-                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.Center
-//            verticalArrangement = Arrangement.SpaceAround   //here at the end space is half as space in b/w
-//            verticalArrangement = Arrangement.SpaceBetween    //here is one colum at one end(top) and other at bottom
-            verticalArrangement = Arrangement.SpaceEvenly   //equal space all round
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround  //other remaining concept are same as the column
+
             ) {
-                Text(text = "hello world")
+                Text(text = "Hello World")
                 Text(text = "Jetpack Compose")
             }
         }
